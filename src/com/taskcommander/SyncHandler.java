5779780316d4f292337f215c7d1ef5c0cfd97c49
Michelle Tan <michelletanjm@gmail.com>
2014-11-05 17:26:52 +0800

    Refactor SyncHandler

--- src/com/taskcommander/SyncHandler.java
+++ src/com/taskcommander/SyncHandler.java
@@ -3,6 +3,7 @@ package com.taskcommander;
 import com.google.api.services.calendar.model.Event;
 
 import java.io.IOException;
+import java.lang.Thread.UncaughtExceptionHandler;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -44,8 +45,9 @@ public class SyncHandler extends Observable {
 	}
 
 	/**
-	 * Syncs the tasks in memory to Google. Checks if tasks have been edited 
-	 * since previous sync and executes respective functions.
+	 * Syncs the tasks in memory to Google in a separate thread. 
+	 * Attempts to login if not already logged in.
+	 * Notifies UI when sync state changes.
 	 * @return   Feedback for user
 	 */
 	public String sync() {
@@ -55,41 +57,59 @@ public class SyncHandler extends Observable {
 			con = GoogleAPIConnector.getInstance();
 		}
 
-		Thread.UncaughtExceptionHandler h = new Thread.UncaughtExceptionHandler() {
-			public void uncaughtException(Thread th, Throwable ex) {
-				logger.log(Level.WARNING, Global.MESSAGE_SYNC_FAILED, ex);
-				th.interrupt();
-				resetSyncState(SyncState.FAILED);
-			}
-		};
-		Thread thread = new Thread() {
-			@Override
-			public void run() {
-				logger.log(Level.INFO, "Waiting for login...");
-				while (!con.getServices()) {
-					try {
-						sleep(10);  // milliseconds
-					} catch (InterruptedException e) {
-						logger.log(Level.WARNING, "Error while trying to sleep in SyncHandler", e);
-					}
-				}
-
-				logger.log(Level.INFO, "Trying to sync...");
-				push();
-				try {
-					pull();
-					resetSyncState(SyncState.DONE);
-				} catch (Exception e) {
-					logger.log(Level.WARNING, Global.MESSAGE_SYNC_FAILED, e);
-				}
-			}
-		};
+		Thread.UncaughtExceptionHandler h = createExceptionHandler();
+		Thread thread = new SyncThread();
 		thread.setUncaughtExceptionHandler(h);
 		thread.start();
 
 		return Global.MESSAGE_SYNC_IN_PROGRESS;
 	}
 
+	/**
+	 * Attempts to get services from GoogleAPIConnector and syncs data to Google.
+	 * If services not available, waits for GoogleAPIConnector to login.
+	 */
+	class SyncThread extends Thread {
+		@Override
+		public void run() {
+			logger.log(Level.INFO, "Waiting for login...");
+			while (!con.getServices()) {
+				try {
+					sleep(10);  // milliseconds
+				} catch (InterruptedException e) {
+					logger.log(Level.WARNING, "Error while trying to sleep in SyncHandler", e);
+				}
+			}
+
+			logger.log(Level.INFO, "Trying to sync...");
+			push();
+			try {
+				pull();
+				resetSyncState(SyncState.DONE);
+			} catch (Exception e) {
+				logger.log(Level.WARNING, Global.MESSAGE_SYNC_FAILED, e);
+			}
+		}
+	}
+	
+	/**
+	 * Returns an UncaughtExceptionHandler that interrupts the thread
+	 * and resets sync state when exceptions are caught.
+	 */
+	private UncaughtExceptionHandler createExceptionHandler() {
+		return new Thread.UncaughtExceptionHandler() {
+			public void uncaughtException(Thread th, Throwable ex) {
+				logger.log(Level.WARNING, Global.MESSAGE_SYNC_FAILED, ex);
+				th.interrupt();
+				resetSyncState(SyncState.FAILED);
+			}
+		};
+	}
+
+	/**
+	 * Pushes tasks that have been changed since last sync to Google services.
+	 * @param tasks
+	 */
 	private void push() {
 		ArrayList<Task> tasks = TaskCommander.data.getAllTasks();
 		ArrayList<Task> deletedTasks = TaskCommander.data.getDeletedTasks();
@@ -99,6 +119,20 @@ public class SyncHandler extends Observable {
 		startSyncState(SyncState.PUSH, tasks.size() + deletedTasks.size() + 
 				preupdatedTasks.size() + clearedTasks.size());
 
+		pushUnsyncedTasks(tasks);
+		pushDeletedTasks(deletedTasks);
+		pushPreupdatedTasks(preupdatedTasks);
+		pushClearedTasks(clearedTasks);
+
+		logger.log(Level.INFO, "PUSH: End Push");
+	}
+
+	/**
+	 * Pushes tasks that have been changed since last sync to Google services.
+	 * Does not include tasks that have changed types.
+	 * @param tasks
+	 */
+	private void pushUnsyncedTasks(ArrayList<Task> tasks) {
 		//Handle Added Cases
 		for (Task t : tasks) {
 			if (!t.isSynced()) {
@@ -114,36 +148,54 @@ public class SyncHandler extends Observable {
 			updateTasksComplete(tasksComplete+1);
 		}
 		logger.log(Level.INFO, "PUSH: Handled Added Cases");
+	}
 
+	/**
+	 * Pushes tasks that have been deleted since last sync to Google services.
+	 * @param deletedTasks
+	 */
+	private void pushDeletedTasks(ArrayList<Task> deletedTasks) {
 		// Handle delete cases
 		for (Task t : deletedTasks) {
 			if (t.getId() != null) {
 				con.deleteTask(t);
 			}
 		}
+		logger.log(Level.INFO, "PUSH: Handled Deleted Cases");
+	}
 
-		// Delete tasks that were updated
+	/**
+	 * Pushes deletions of tasks that have changed task type 
+	 * since last sync to Google services.
+	 * @param preupdatedTasks
+	 */
+	private void pushPreupdatedTasks(Stack<Task> preupdatedTasks) {
+		// Delete tasks that were updated to a different type
 		for (Task t: preupdatedTasks) {
 			if (t.getId() != null) {
 				con.deleteTask(t);
 			}
 		}
+		logger.log(Level.INFO, "PUSH: Handled Preupdated Cases");
+	}
 
-
+	/**
+	 * Pushes tasks that have been cleared since last sync to Google services.
+	 * @param clearedTasks
+	 */
+	private void pushClearedTasks(Stack<ArrayList<Task>> clearedTasks) {
 		// Delete tasks that were cleared
 		for (ArrayList<Task> list : clearedTasks) {
-			for (Task t : list) {
-				if (t.getId() != null) {
-					con.deleteTask(t);
-				}
-			}
+			pushDeletedTasks(list);
 		}
-
-		logger.log(Level.INFO, "PUSH: Handled Deleted Cases");
-		logger.log(Level.INFO, "PUSH: End Push");
+		logger.log(Level.INFO, "PUSH: Handled Cleared Cases");
 	}
 
 	//@author A0109194A
+	/**
+	 * Pulls tasks that have been changed since last sync to Google services.
+	 * @param tasks
+	 */
 	private void pull() throws IOException {
 		logger.log(Level.INFO, "PULL: Starting Pull");
 		//Get all Tasks
@@ -153,19 +205,38 @@ public class SyncHandler extends Observable {
 		List<Event> googleEvents = con.getAllGoogleEvents();
 		ArrayList<String> taskIds = TaskCommander.data.getAllIds();
 		logger.log(Level.INFO, "PULL: Retrieved All Tasks");
+
 		startSyncState(SyncState.PULL, getTotalTasks(tasksToSync, tasks, googleTasks, googleEvents));
 
-		//Added case
-		//For Tasks
-		if (googleTasks != null) {
-			for (com.google.api.services.tasks.model.Task task : googleTasks) {
-				if (!taskIds.contains(task.getId()) && task.getDeleted() == null) {
-					TaskCommander.data.addTask(con.toTask(task));
-				}
-				updateTasksComplete(tasksComplete+1);
-			}
-		}
+		pullAddedCases(googleTasks, googleEvents, taskIds);
+		pullUpdatedCases(tasksToSync);
+		pullDeletedCases(googleTasks, googleEvents);
 
+		logger.log(Level.INFO, "PULL: End Pull");
+	}
+
+	/**
+	 * Pulls new tasks from Google services that have been
+	 * added since last sync, and adds them locally.
+	 * @param googleTasks
+	 * @param googleEvents
+	 * @param taskIds
+	 */
+	private void pullAddedCases(List<com.google.api.services.tasks.model.Task> googleTasks,
+			List<Event> googleEvents, ArrayList<String> taskIds) {
+		pullAddedCasesForTasks(googleTasks, taskIds);
+		pullAddedCasesForCalendar(googleEvents, taskIds);
+		logger.log(Level.INFO, "PULL: Handled Added Tasks");
+	}
+
+	/**
+	 * Pulls new tasks from Google Calendar service that have been
+	 * added since last sync, and adds them locally.
+	 * @param googleEvents
+	 * @param taskIds
+	 */
+	private void pullAddedCasesForCalendar(List<Event> googleEvents,
+			ArrayList<String> taskIds) {
 		if (googleEvents != null) {
 			//For Events
 			for (Event event: googleEvents) {
@@ -175,11 +246,35 @@ public class SyncHandler extends Observable {
 				updateTasksComplete(tasksComplete+1);
 			}
 		}
+	}
 
-		logger.log(Level.INFO, "PULL: Handled Added Tasks");
+	/**
+	 * Pulls new tasks from Google Tasks service that have been
+	 * added since last sync, and adds them locally.
+	 * @param googleTasks
+	 * @param taskIds
+	 */
+	private void pullAddedCasesForTasks(
+			List<com.google.api.services.tasks.model.Task> googleTasks,
+			ArrayList<String> taskIds) {
+		if (googleTasks != null) {
+			for (com.google.api.services.tasks.model.Task task : googleTasks) {
+				if (!taskIds.contains(task.getId()) && task.getDeleted() == null) {
+					TaskCommander.data.addTask(con.toTask(task));
+				}
+				updateTasksComplete(tasksComplete+1);
+			}
+		}
+	}
 
-
-
+	/**
+	 * Pulls tasks from Google services that have been
+	 * updated since last sync, and updates them locally.
+	 * @param tasksToSync
+	 */
+	private void pullUpdatedCases(ArrayList<Task> tasksToSync) {
+		ArrayList<Task> tasks;
+		ArrayList<String> taskIds;
 		if (tasksToSync != null) {
 			//Updated cases
 			tasks = TaskCommander.data.getAllTasks();
@@ -206,11 +301,29 @@ public class SyncHandler extends Observable {
 			}
 		}
 		logger.log(Level.INFO, "PULL: Handled Updated Cases");
+	}
 
-		//Deleted case
-		//For Tasks
+	/**
+	 * Pulls tasks from Google services that have been
+	 * deleted since last sync, and deletes them locally.
+	 * @param googleTasks
+	 * @param googleEvents
+	 */
+	private void pullDeletedCases(List<com.google.api.services.tasks.model.Task> googleTasks,
+			List<Event> googleEvents) {
+		pullDeletedCasesForTasks(googleTasks);
+		pullDeletedCasesForCalendar(googleEvents);
+	}
+
+	/**
+	 * Pulls tasks from Google Tasks service that have been
+	 * deleted since last sync, and deletes them locally.
+	 * @param googleTasks
+	 */
+	private void pullDeletedCasesForTasks(
+			List<com.google.api.services.tasks.model.Task> googleTasks) {
+		ArrayList<String> taskIds;
 		if (googleTasks != null) {
-			tasks = TaskCommander.data.getAllTasks();
 			taskIds = TaskCommander.data.getAllIds();
 			for (com.google.api.services.tasks.model.Task t : googleTasks) {
 				if (t.getDeleted() != null) {
@@ -224,11 +337,17 @@ public class SyncHandler extends Observable {
 				updateTasksComplete(tasksComplete+1);
 			}
 		}
-
 		logger.log(Level.INFO, "PULL: Handled Deleted Google Tasks");
+	}
 
+	/**
+	 * Pulls tasks from Google Calendar service that have been
+	 * deleted since last sync, and deletes them locally.
+	 * @param googleEvents
+	 */
+	private void pullDeletedCasesForCalendar(List<Event> googleEvents) {
+		ArrayList<String> taskIds;
 		if (googleEvents != null) {
-			tasks = TaskCommander.data.getAllTasks();
 			taskIds = TaskCommander.data.getAllIds();
 			//Deleted Case For Events
 			for (Event event : googleEvents) {
@@ -244,9 +363,16 @@ public class SyncHandler extends Observable {
 			}
 		}
 		logger.log(Level.INFO, "PULL: Handled Deleted Google Events");
-		logger.log(Level.INFO, "PULL: End Pull");
 	}
 
+	/** 
+	 * Gets total number of tasks from Google services to update locally 
+	 * @param tasksToSync
+	 * @param tasks
+	 * @param googleTasks
+	 * @param googleEvents
+	 * @return               Total number of tasks from Google services to update
+	 */
 	private int getTotalTasks(ArrayList<Task> tasksToSync,
 			ArrayList<Task> tasks,
 			List<com.google.api.services.tasks.model.Task> googleTasks,
