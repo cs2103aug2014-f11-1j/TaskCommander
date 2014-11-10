7f46d997f66d1b5ebb216183ec374198b4d29f34
Michelle Tan <michelletanjm@gmail.com>
2014-11-10 20:44:03 +0800

    Add User Guide Link to Help Tab

--- src/com/taskcommander/UI.java
+++ src/com/taskcommander/UI.java
@@ -123,7 +123,8 @@ public class UI extends Observable implements Observer {
 			"sync \n" +
 			"sort \n" +
 			"undo \n" +
-			"exit \n";
+			"exit \n\n"+
+			"For more help, you can access our user guide here: http://goo.gl/6bjc7i";
 	private static final String INSTRUCTIONS_MAIN = "Enter command: ";
 	private static final String INSTRUCTIONS_BROWSER = "Please login to Google and accept application permissions to sync your tasks.";
 	
